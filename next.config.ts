7932de23b261565afd00ad783b6dceabe99a4e86
Chungwon Jung <wjdcjddnjs1234@gmail.com>
2026-02-26 02:26:59 +0900

    ref config

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' https://api.oppa-recipe.shop http://localhost:8080;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; connect-src 'self' https://api.oppa-recipe.shop http://localhost:8080; frame-src 'self' https://www.youtube.com https://youtube.com;",
           },
         ],
       },
