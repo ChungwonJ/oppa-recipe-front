aeeb37594d92ad91ba1bd3c5c9b26a1327159ae7
Chungwon Jung <wjdcjddnjs1234@gmail.com>
2026-02-26 02:01:32 +0900

    app test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,14 +12,20 @@ export default function App({ Component, pageProps }: AppProps) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const { accessToken } = router.query;
+    const { accessToken, id } = router.query;
 
     if (accessToken) {
       localStorage.setItem('accessToken', accessToken as string);
+      if (id) {
+        localStorage.setItem('userId', id as string);
+      }
+      
       setIsLoggedIn(true);
+
       router.replace('/', undefined, { shallow: true });
       return;
     }
+
     const savedToken = localStorage.getItem('accessToken');
     if (savedToken) {
       setIsLoggedIn(true);
